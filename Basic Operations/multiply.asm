4f5601b7dc9194e6c67d90f36ec009f29d9fdcd0
Fernando López Ramírez <39963559+Fernando74lr@users.noreply.github.com>
2021-02-12 13:41:56 -0600

    Create multiply.asm

---
+++ Basic Operations/multiply.asm
@@ -0,0 +1,27 @@
+/ Prompt user to type in integers
+INPUT
+Store X
+INPUT
+Store Y
+
+/ Loop for performing iterative addition
+loop,  Load num
+       Add X
+       Store num
+       Load Y
+       Subt one
+       Store Y
+       Skipcond 400 / have we completed the multiplication?
+       Jump loop / no; repeat loop
+       / yes, so exit the loop
+       
+/ Output result to user then halt program
+Load num
+Output
+Halt
+
+/ Declare labels here
+X, DEC 0
+Y, DEC 0
+one, DEC 1
+num, DEC 0
