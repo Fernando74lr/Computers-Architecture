8f3e143e1ebca87c6e3185302717133c7dd8319e
Fernando López Ramírez <39963559+Fernando74lr@users.noreply.github.com>
2021-02-17 21:57:15 -0600

    Create Act_1.2.asm

---
+++ Activities/Act_1.2.asm
@@ -0,0 +1,24 @@
+/ Activity 1.2: add 5 numbers
+
+add, Input / Ask for input
+    Store NUM / Store input value in NUM
+    
+    Load RES / Load RES in AC
+    Add NUM / Add NUM to AC
+    Store RES / Store AC in RES
+    
+    Load TIMES / Load TIMES in AC
+    Subt ONE / Substract ONE to AC
+    Store TIMES / Store AC in TIMES
+    
+    Skipcond 400 / Skip if AC is equal to 0
+    Jump add / Jump to "add" subroutine
+
+Load RES
+Output
+Halt
+
+ONE, DEC 1
+NUM, DEC 0
+RES, DEC 0
+TIMES, DEC 5
